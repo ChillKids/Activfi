03b6fe7a2daddfb448ad77c77da89174c4d2211a
Joe <iwillridgley@gmail.com>
2019-09-21 11:35:08 -0600

    added basic functionality for ui

---
+++ activfi/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {AppBar} from '@material-ui/core'
+function App() {
+
+
+  return (
+    <AppBar>
+     <Button variant="contained" color="primary">
+       Hello World
+     </Button> 
+     </AppBar>
+  );
+}
+
+export default App;
