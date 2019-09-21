aa0dc9dc7e10f2f121af36ca005c22b0d4739f47
Jonathan <jonathanman2010@icloud.com>
2019-09-21 05:21:57 -0600

    modified cors code

--- app.js
+++ app.js
@@ -40,9 +40,12 @@ app.use(function(req, res, next) {
 });
 // app.use(cors(corsOptions));
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) {
     res.header('Access-Control-Allow-Origin', `${CORS_URL}`);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+})
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
