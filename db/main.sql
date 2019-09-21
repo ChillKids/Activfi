9aab2dded72e9cb0c8016daced2438f870efe1a2
Jonathan <jonathanman2010@icloud.com>
2019-09-20 20:40:33 -0600

    added db/main.sql

---
+++ db/main.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS events CASCADE;
+
+CREATE TABLE events (
+  event_id serial PRIMARY KEY,
+  event_creator VARCHAR,
+  event_name VARCHAR,
+  event_description VARCHAR,
+  event_location VARCHAR,
+  event_date DATE,
+  created_at DATE
+);
+
