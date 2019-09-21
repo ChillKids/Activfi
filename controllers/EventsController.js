63e1e1e824609f9b2015ceaf56737358580d8942
Betzer09 <betzer.austin09@gmail.com>
2019-09-20 20:19:26 -0600

    Added events model and model controller. Install ps to interact with db

---
+++ controllers/EventsController.js
@@ -0,0 +1,31 @@
+// Call the events models function
+
+const eventsRouter = require('../models/events');
+
+// Gets the event from the given id 
+function getEventWith(id) {
+
+}
+
+// Creates and event
+function createEvent(event) {
+
+}
+
+function updateEvent(event) {
+
+}
+
+function deleteEvent(event) {
+
+}
+
+
+
+
+
+module.exports = {
+    getLoadIds: getLoadIds,
+    getLoad: getLoad,
+    getLoads: getLoads
+}
